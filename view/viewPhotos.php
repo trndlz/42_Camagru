a20c16e5a1ffcd41471402f7cae876190a97cd08
Trndlz <trestan.mervin@gmail.com>
2018-07-07 23:51:04 +0200

    Minor changes

--- view/viewPhotos.php
+++ view/viewPhotos.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once('helpers/timeago.php');
 
 function displayPhotos($array) {
@@ -17,8 +18,6 @@ function displayPhotos($array) {
 		</div>
 		<p class='img_info'>Posted from <a href='#'><?= htmlspecialchars($line['auteur']) ?></a>
 		<?= get_timeago(strtotime($line['date'])); ?></p>
-
-		<p>&hearts; - Comment - Share - Love</p>
 	</div>
 <?php
 	}
