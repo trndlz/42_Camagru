bf980dd5bddb6efe4bf2703bad2c05f4730bf2eb
Trestan MERVIN <trestan.mervin@gmail.com>
2018-08-14 20:16:37 +0200

    Layout webcam

--- public/js/webcam.js
+++ public/js/webcam.js
@@ -1,41 +1,47 @@
 
 
-navigator.mediaDevices.getUserMedia({ audio: false, video: true })
+navigator.mediaDevices.getUserMedia({ audio: false, video: { width: 640, height: 640 } })
 .then(function(stream) {
     var video = document.getElementById('videoElement');
     var snap = document.getElementById('snap');
     var canvas = document.getElementById("canvas");
     var canvas_copy = document.getElementById("canvas_copy");
-    var start = document.getElementById("start_rec");
-    var stop = document.getElementById("freeze_rec");
     var gray = document.getElementById("gray");
-    var no = document.getElementById("no_effect");
+    var no_effect = document.getElementById("no_effect");
+    var invert = document.getElementById("invert");
     var ctx = canvas.getContext('2d');
+    var videoWidth, videoHeight;
+    var filter = '';
     
     video.srcObject = stream;
     video.onloadedmetadata = function() {
         snap.disabled = false;
-        start.onclick = function() {
-            video.play();
-        }
-        stop.onclick = function() {
-            video.pause();
-        }
+        videoWidth = video.videoWidth;
+        videoHeight = video.videoHeight;
+        canvas.width  = videoWidth;
+        canvas.height = videoHeight;
+        canvas_copy.width  = videoWidth;
+        canvas_copy.height = videoHeight;
+        video.play();
         snap.onclick = function() {
-            // if (!video.paused) {
-            //     console.log("La video est lancée");
-            // }
-            canvas.getContext('2d').drawImage(video, 0, 0, 300, 300, 0, 0, 300, 300);
-            canvas_copy.getContext('2d').drawImage(canvas, 0, 0, 300, 300, 0, 0, 300, 300);
+            ctx.filter = filter;
+            canvas.getContext('2d').drawImage(video, 0, 0, videoWidth, videoHeight, 0, 0, videoWidth, videoHeight);
+            canvas_copy.getContext('2d').drawImage(video, 0, 0, videoWidth, videoHeight, 0, 0, videoWidth, videoHeight);
+            // ctx.filter = 'grayscale(100%)';
+            // canvas.getContext('2d').drawImage(video, 0, 0, videoWidth, videoHeight, 0, 0, videoWidth, videoHeight);
+            // canvas_copy.getContext('2d').drawImage(video, 0, 0, videoWidth, videoHeight, 0, 0, videoWidth, videoHeight);
         }
         gray.onclick = function() {
-            
-            ctx.filter = 'grayscale(100%)';
-            canvas.getContext('2d').drawImage(video, 0, 0, 300, 300, 0, 0, 300, 300);
+            filter = 'grayscale(100%)';
+            video.style.filter = filter;
         }
         no_effect.onclick = function() {
-            ctx.filter = 'none';
-            canvas.getContext('2d').drawImage(canvas_copy, 0, 0, 300, 300, 0, 0, 300, 300);
+            filter = 'none';
+            video.style.filter = filter;
+        }
+        invert.onclick = function() {
+            filter = 'invert(100%)';
+            video.style.filter = filter;
         }
     };
 })
--- view/addphotos.php
+++ view/addphotos.php
@@ -1,11 +1,15 @@
-        
-        <h1>Take a picture !</h1>
+        <h1 id="cam_titles">Take a photo !</h1>
         <video id="videoElement"></video>
-        <input id="snap" type="button" disabled="true" value="Snapshot" class="webcam-button"></input>
-        <br><input id="start_rec" type="button" value="Start video stream" class="webcam-button"></input>
-        <input id="freeze_rec" type="button" value="Freeze" class="webcam-button"></input>
-        <input id="no_effect" type="button" value="No effect" class="webcam-button"></input>
-        <input id="gray" type="button" value="Gray scale" class="webcam-button"></input>
-        <canvas id="canvas"></canvas>
+        <p class="button_p">
+            <input id="snap" type="button" disabled="true" value="Snapshot" class="webcam-button"></input>
+            <input id="no_effect" type="button" value="No effect" class="webcam-button"></input>
+            <input id="gray" type="button" value="Gray scale" class="webcam-button"></input>
+            <input id="invert" type="button" value="Invert" class="webcam-button"></input>
+        </p>
+        <h1 id="cam_titles">Add filters ?</h1>
+        <canvas id="canvas" width="300" height="300"></canvas>
+        <p class="button_p">
+            
+        </p>
         <canvas id="canvas_copy" width="300" height="300"></canvas>
         <script src="public/js/webcam.js"></script>
