62ea8c9d3f01437c0044509f2ec0e38218414f64
Trestan MERVIN <trestan.mervin@gmail.com>
2018-08-15 12:07:18 +0200

    Added tree JS superposition

--- public/js/webcam.js
+++ public/js/webcam.js
@@ -1,6 +1,14 @@
+var header = document.getElementById("trees");
+var i_trees = header.getElementsByClassName("i_trees");
+for (var i = 0; i < i_trees.length; i++) {
+  i_trees[i].addEventListener("click", function() {
+    active_photo = document.getElementsByClassName("active");
+    active_photo[0].className = active_photo[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
 
-
-navigator.mediaDevices.getUserMedia({ audio: false, video: { width: 640, height: 640 } })
+navigator.mediaDevices.getUserMedia({ audio: false, video: true })
 .then(function(stream) {
     var video = document.getElementById('videoElement');
     var snap = document.getElementById('snap');
@@ -9,6 +17,8 @@ navigator.mediaDevices.getUserMedia({ audio: false, video: { width: 640, height:
     var gray = document.getElementById("gray");
     var no_effect = document.getElementById("no_effect");
     var invert = document.getElementById("invert");
+    var tree0 = document.getElementById('tree0');
+    var tree1 = document.getElementById('tree1');
     var ctx = canvas.getContext('2d');
     var videoWidth, videoHeight;
     var filter = '';
@@ -22,14 +32,17 @@ navigator.mediaDevices.getUserMedia({ audio: false, video: { width: 640, height:
         canvas.height = videoHeight;
         canvas_copy.width  = videoWidth;
         canvas_copy.height = videoHeight;
+        var selected_tree = 0;
         video.play();
         snap.onclick = function() {
             ctx.filter = filter;
+            var header = document.getElementById("trees");
+            var selected_tree = header.getElementsByClassName("active");
+            console.log(active_photo);
+            console.log('coucou');
             canvas.getContext('2d').drawImage(video, 0, 0, videoWidth, videoHeight, 0, 0, videoWidth, videoHeight);
+            canvas.getContext('2d').drawImage(selected_tree[0], 0, 0, videoWidth, videoHeight, 0, 0, videoWidth, videoHeight);
             canvas_copy.getContext('2d').drawImage(video, 0, 0, videoWidth, videoHeight, 0, 0, videoWidth, videoHeight);
-            // ctx.filter = 'grayscale(100%)';
-            // canvas.getContext('2d').drawImage(video, 0, 0, videoWidth, videoHeight, 0, 0, videoWidth, videoHeight);
-            // canvas_copy.getContext('2d').drawImage(video, 0, 0, videoWidth, videoHeight, 0, 0, videoWidth, videoHeight);
         }
         gray.onclick = function() {
             filter = 'grayscale(100%)';
--- view/addphotos.php
+++ view/addphotos.php
@@ -1,3 +1,8 @@
+        <h1 id="cam_titles">Choose your tree !</h1>
+        <div id="trees">
+            <img src="public/img/tree1.png" class="i_trees active" width="25%">
+            <img src="public/img/tree2.png" class="i_trees" width="25%">
+        </div>
         <h1 id="cam_titles">Take a photo !</h1>
         <video id="videoElement"></video>
         <p class="button_p">
@@ -8,8 +13,9 @@
         </p>
         <h1 id="cam_titles">Add filters ?</h1>
         <canvas id="canvas" width="300" height="300"></canvas>
+        <h1 id="cam_titles">Happy ?</h1>
         <p class="button_p">
-            
+            <input id="snap" type="button" disabled="true" value="Submit" class="webcam-button"></input>
         </p>
         <canvas id="canvas_copy" width="300" height="300"></canvas>
         <script src="public/js/webcam.js"></script>
